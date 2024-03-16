15bc901876e1d7da2c34f18b93a9901745ca215c
irshad500 <mohammedirshadkp500@gmail.com>
2024-03-16 09:25:44 +0530

    game updated

--- lib/game/managers/object_manager.dart
+++ lib/game/managers/object_manager.dart
@@ -46,7 +46,7 @@ class ObjectManager extends Component with HasGameRef<CarRace> {
 
   final List<EnemyPlatform> _enemies = [];
   void _maybeAddEnemy() {
-    if (specialPlatforms['eemy'] != true) {
+    if (specialPlatforms['neemy'] != true) {
       return;
     }
 
